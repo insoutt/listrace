15b42fbe2bf18ec4c71112cb3ee3224d4d35026a
insoutt <elvisfernando9@hotmail.com>
2024-06-18 15:39:41 -0500

    Improve search

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,4 +1,4 @@
-import { NEWS, PLACES, REVIEWS, SEARCH } from "@/data";
+import { SEARCH } from "@/data";
 import { mockResponseTime } from "@/lib/utils";
 
 export async function POST(
@@ -17,8 +17,8 @@ export async function POST(
   }
 
   let items = SEARCH
-                .filter(item => item.type.toLocaleLowerCase().indexOf(type) > -1)
-                .filter(item => item.title.toLocaleLowerCase().indexOf(place) > -1)
+                .filter(item => item.type.toLocaleLowerCase().trim().indexOf(type.toLocaleLowerCase()) > -1)
+                .filter(item => item.title.toLocaleLowerCase().trim().indexOf(place.toLocaleLowerCase()) > -1)
 
   return Response.json({ data: items });
 }
